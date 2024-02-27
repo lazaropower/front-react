1b62fd02ff9250f017ef373ae35bd569e9c56e05
Lázaro <20268108+lazaropower@users.noreply.github.com>
2024-02-27 15:19:43 +0200

    Add and configure tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline">
+          Hello World
+        </h1>
       </header>
     </div>
   );
